ea00d2e4ea4b2b9f81a2e37d13fa500d39b380b7
NP-SON-250 <hakizimanaalexis@gmail.com.com>
2025-11-26 11:31:47 +0200

    Remove description

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -21,11 +21,6 @@ const LeftBanner = () => {
           a <span className="text-secondary lg:text-4xl text-2xl">{text}</span>
           <Cursor cursorBlinking="false" cursorStyle="|" cursorColor="#ffff" />
         </h2>
-        <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
-        </p>
       </div>
       {/* Media */}
       <Media />
